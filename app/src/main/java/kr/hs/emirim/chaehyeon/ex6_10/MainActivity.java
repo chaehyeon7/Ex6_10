a62472bade22b61b28d8d53a4ef1495d550e1c89
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-06-03 19:06:12 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/chaehyeon/ex6_10/MainActivity.java
@@ -0,0 +1,30 @@
+package kr.hs.emirim.chaehyeon.ex6_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] items = {"Cat Tower", "Car Center", "Car Racing", "Android", "Ant House", "Sound Track", "Sono Vilige"};
+
+        AutoCompleteTextView autoText = findViewById(R.id.auto_text);
+        MultiAutoCompleteTextView multiText = findViewById(R.id.multi_text);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line);
+        autoText.setAdapter(adapter);
+
+        MultiAutoCompleteTextView.CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
+        multiText.setTokenizer(token);
+        multiText.setAdapter(adapter);
+    }
+}
